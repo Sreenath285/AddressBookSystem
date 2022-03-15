094cc4aed0365d7894726de4f6d9f23d90b99d1c
Sreenath285 <sreenath.285@gmail.com>
2022-03-15 17:13:53 +0530

    Editing contact

---
+++ AddressBookSystem/src/AddressBook.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    List<Contact> contactList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    /***
+     * created readLongInput method to read long input
+     * @param inputMsg - passing input message
+     * @return - returning input
+     */
+    public long readLongInput(String inputMsg) {
+        System.out.print(inputMsg);
+        long inputLong = sc.nextLong();
+        return  inputLong;
+    }
+
+    /***
+     * created readStringInput method to read long input
+     * @param inputMsg - passing input message
+     * @return - returning input
+     */
+    public String readStringInput(String inputMsg) {
+        System.out.print(inputMsg);
+        String inputStr = sc.next();
+        return  inputStr;
+    }
+
+    /***
+     * created addPerson method to enter contact details from user
+     */
+    public void addPerson() {
+        String firstName = readStringInput("Enter first name : ");
+        String lastName = readStringInput("Enter last name : ");
+        String address = readStringInput("Enter address : ");
+        String city = readStringInput("Enter city : ");
+        String state = readStringInput("Enter state : ");
+        String email = readStringInput("Enter email : ");
+        long phoneNumber = readLongInput("Enter phone number : ");
+        long zipCode = readLongInput("Enter zip code : ");
+
+        Contact contact =new Contact(firstName, lastName, address, city, state, email, phoneNumber, zipCode);
+        contactList.add(contact);
+
+        System.out.println("Successfully added");
+    }
+
+    /***
+     * created editContact method to edit contact
+     * calling findContact method to find the contact by giving user's first name
+     */
+    public void editContacts() {
+        Contact contact = findContact();
+        String continueEdit = "";
+        do {
+            System.out.println("1. Edit first name \n2. Edit last name \n3. Edit address" +
+                    "\n4. Edit city \n5. Edit state \n6. Edit email" +
+                    "\n7. Edit zip code \n8. Edit phone number");
+            System.out.print("Enter choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1 :
+                    contact.setFirstName(readStringInput("Enter first name : "));
+                    break;
+                case 2 :
+                    contact.setLastName(readStringInput("Enter last name : "));
+                    break;
+                case 3 :
+                    contact.setAddress(readStringInput("Enter address : "));
+                    break;
+                case 4 :
+                    contact.setCity(readStringInput("Enter city : "));
+                    break;
+                case 5 :
+                    contact.setEmail(readStringInput("Enter state : "));
+                    break;
+                case 6 :
+                    contact.setEmail(readStringInput("Enter email : "));
+                    break;
+                case 7 :
+                    contact.setZipCode(readLongInput("Enter zip code : "));
+                    break;
+                case 8 :
+                    contact.setPhoneNumber(readLongInput("Enter phone number : "));
+                    break;
+                default :
+                    System.out.println("Invalid input");
+                    break;
+            }
+            System.out.print("Do you want to continue (y/n) :");
+            continueEdit = sc.next();
+        }
+        while (continueEdit.equalsIgnoreCase("Y"));
+    }
+
+    /***
+     * created findContact method to find the contact by giving user's first name
+     * @return - returning contact
+     */
+    private Contact findContact() {
+        System.out.print("Enter first name : ");
+        String firstName = sc.next();
+        int temp = 0;
+        Contact tempContact = null;
+        for (Contact cont : contactList) {
+            if (cont.getFirstName().equals(firstName)) {
+                temp++;
+                tempContact = cont;
+            }
+        }
+        if (temp == 1) {
+            return tempContact;
+        }
+        else {
+            System.out.println("There is no contact with the given first name");
+            System.out.println("Enter correct first name");
+            findContact();
+        }
+        return tempContact;
+    }
+
+    /***
+     * created showContacts method to show contacts
+     */
+    public void showContacts() {
+        for (Contact cont : contactList) {
+            System.out.println("--------------------------------------------");
+            System.out.println("First name      : " + cont.getFirstName());
+            System.out.println("Last name       : " + cont.getLastName());
+            System.out.println("Address         : " + cont.getAddress());
+            System.out.println("City            : " + cont.getCity());
+            System.out.println("State           : " + cont.getState());
+            System.out.println("Email           : " + cont.getEmail());
+            System.out.println("Phone number    : " + cont.getPhoneNumber());
+            System.out.println("Zip code        : " + cont.getZipCode());
+        }
+        if (contactList.isEmpty()) {
+            System.out.println("Address book is empty");
+        }
+    }
+}
---
+++ AddressBookSystem/src/AddressBookMain.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    /***
+     * main method for manipulation
+     * @param args - default param(not used)
+     */
+    public static void main(String[] args) {
+        /***
+         * PROCEDURE
+         * 1. creating addressBook
+         * 2. iterating while loop for choice
+         */
+
+        /***
+         * 1. creating addressBook
+         */
+        AddressBook addressBook = new AddressBook();
+        /***
+         * 2. iterating while loop for choice
+         */
+        while (true) {
+            System.out.println("-------------------------------------------");
+            System.out.println("1. Add person \n2. Edit contact \n3. Show contact\n4. Exit");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1 :
+                    addressBook.addPerson();
+                    break;
+                case 2 :
+                    addressBook.editContacts();
+                    break;
+                case 3 :
+                    addressBook.showContacts();
+                    break;
+                case 4 :
+                    System.exit(0);
+                    break;
+                default :
+                    break;
+            }
+        }
+    }
+}
