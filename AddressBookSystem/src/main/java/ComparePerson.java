4a58405e200e8d6d70ddb0d25b3bc5719333e98d
Sreenath285 <70939307+Sreenath285@users.noreply.github.com>
2022-03-23 14:01:58 +0530

    Delete AddressBookSystem directory

--- AddressBookSystem/src/main/java/ComparePerson.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparePerson implements Comparator<Contact> {
-    @Override
-    public int compare(Contact person1, Contact person2) {
-        return person1.getFirstName().compareTo(person2.getFirstName());
-    }
-}
